bc1af5f2a839269061d9a63e521c54a87b8c880d
Emmanuel Zelarayan <emmavz@live.com>
2022-07-23 19:55:48 -0300

    initial files, set controllers, models and routes

---
+++ controllers/products.js
@@ -0,0 +1,13 @@
+const getAllProductsStatic = async (req, res) => {
+  throw new Error("testing async errors");
+  res.status(200).json({ msg: "products testing route" });
+};
+
+const getAllProducts = async (req, res) => {
+  res.status(200).json({ msg: "products route" });
+};
+
+module.exports = {
+  getAllProducts,
+  getAllProductsStatic,
+};
